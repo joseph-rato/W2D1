66a2f85c1d7f386280c07f82952b2cdc62cf74c5
Joseph <Rato>
2018-08-27 15:33:49 -0700

    finish Class inheritance

---
+++ Class_inheritance.rb
@@ -0,0 +1,39 @@
+class Employee
+  
+  attr_reader :salary
+  
+  def initialize(name, title, salary, boss)
+    @name = name
+    @title = title 
+    @salary = salary  
+    @boss = boss
+  end 
+  
+  def bonus(multiplier)
+    @salary * multiplier
+  end 
+  
+  def boss=(boss)
+    boss.employees << self
+    @boss = value
+  end 
+  
+end 
+class Manager < Employee
+  
+  attr_accessor :employees 
+  
+  def initialize(name, title, salary, boss)
+    super
+    @employees = []
+  end 
+  
+  def bonus(multiplier)
+    total = 0
+    @employees.each do |employee| 
+      total += employee.salary
+    end 
+    multiplier * total 
+  end 
+  
+end 
