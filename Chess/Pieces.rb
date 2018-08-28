8b78ab1d08b186ccc35dece6dd31a2d51b9936f0
Joseph <Rato>
2018-08-27 17:30:55 -0700

    start display class and piece class

--- Chess/Pieces.rb
+++ Chess/Pieces.rb
@@ -1,13 +1,22 @@
 class Piece 
   
-  attr_reader :color
+  attr_reader :color, :pos
   
   def initialize(color=nil)
     @color = color
   end 
   
+  def valid_move?(start_pos, end_pos)
+    true
+  end 
+  
 end
 class Rook < Piece 
+  
+  def move_dirs
+  
+  end 
+  
 end 
 class Bishop < Piece 
 end 
@@ -21,3 +30,7 @@ class Pawn < Piece
 end 
 class NullPiece < Piece 
 end
+
+# Why do we need to store pos within each piece 
+# evaluating our board valid_move? logic 
+# 
